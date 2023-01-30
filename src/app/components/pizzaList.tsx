00e909afeaa2dab26bd02edb12bfb6afe92e02d2
Akira <antonov.egor@yandex.ru>
2023-01-30 17:01:48 +0700

    third commit

--- src/app/components/pizzaList.tsx
+++ src/app/components/pizzaList.tsx
@@ -1,5 +1,6 @@
 import React, { Component, FC } from 'react'
 import PizzaItem from './pizzaItem'
+import Form from './form'
 export interface IPizzaItem {
     classifications: {
         new: boolean,
@@ -31,21 +32,25 @@ interface PizzaList {
 
 const PizzaList: FC<PizzaList> = ({pizza}) => {
     return ( 
-        <div>
-            <ul className='pizza-list grid grid-cols-6 gap-4 mb-1'>
-                {
-                    pizza.map((item) => <PizzaItem key={item.id} pizza={item}  />)
-                }
-            </ul>
-            <div className="flex space-x-2 justify-center">
-                <button
-                    type="button"
-                    data-mdb-ripple="true"
-                    data-mdb-ripple-color="light"
-                    className="rounded-md text-2xl inline-block px-6 py-4 bg-yellow-500 text-white font-medium text-xs leading-tight uppercase shadow-md hover:bg-yellow-500 hover:shadow-lg focus:bg-yellow-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-500 active:shadow-lg transition duration-150 ease-in-out"
-                >Заказать пиццу</button>
+        <>
+            <div>
+                <ul className='pizza-list grid grid-cols-6 md:grid-cols-4 gap-4 mb-1'>
+                    {
+                        pizza.map((item) => <PizzaItem key={item.id} pizza={item}  />)
+                    }
+                </ul>
+                <div className="flex space-x-2 justify-center">
+                    <button
+                        type="button"
+                        data-mdb-ripple="true"
+                        data-mdb-ripple-color="light"
+                        className="rounded-md text-2xl inline-block px-6 py-4 bg-yellow-500 text-white font-medium text-xs leading-tight uppercase shadow-md hover:bg-yellow-500 hover:shadow-lg focus:bg-yellow-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-500 active:shadow-lg transition duration-150 ease-in-out"
+                    >Заказать пиццу</button>
+                </div>
             </div>
-        </div>
+            <Form />
+        </>
+
     );
 }
  
